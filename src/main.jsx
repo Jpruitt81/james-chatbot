94e14c248bae255857d9503c47010054f38491cf
Jpruitt81 <pruitt.james81@yahoo.com>
2025-07-11 09:26:53 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ChatBotJames from './ChatBotJames.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChatBotJames />
+  </React.StrictMode>
+);
