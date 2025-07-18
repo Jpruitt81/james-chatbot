48b9dafb11e1978fa1c3bb9fb270bc5704221049
Jpruitt81 <pruitt.james81@yahoo.com>
2025-07-18 08:24:31 +0000

    Add Tailwind CSS support

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ChatBotJames from './ChatBotJames.jsx';
+import ChatBotJames from '../ChatBotJames.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
